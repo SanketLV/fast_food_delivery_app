17640ee70ddda6c82ee8a284c3e4fe7166634719
SanketLV <sanketlakhani80@gmail.com>
2025-07-11 19:19:54 +0530

    cart and logout functionality added

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,11 +1,20 @@
 import {View, Text} from 'react-native'
 import React from 'react'
+import {SafeAreaView} from "react-native-safe-area-context";
+import CustomButton from "@/components/CustomButton";
+import {account} from "@/lib/appwrite";
+import {router} from "expo-router";
 
 const Profile = () => {
+    const handleLogout = async () => {
+        await account.deleteSession("current")
+        router.push("/sign-in")
+    }
+
     return (
-        <View>
-            <Text>Profile</Text>
-        </View>
+        <SafeAreaView className="flex items-end justify-center h-full">
+            <CustomButton title="Logout" onPress={handleLogout}/>
+        </SafeAreaView>
     )
 }
 export default Profile
